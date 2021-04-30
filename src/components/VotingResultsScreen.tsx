3ab97e5fe838eacc4e8aa58d82704dda19a067e8
SpiderBeaver <spdrbeaver@gmail.com>
2021-04-30 13:24:59 +0300

    Style VotingResultsScreen.

--- src/components/VotingResultsScreen.tsx
+++ src/components/VotingResultsScreen.tsx
@@ -1,35 +1,119 @@
 import React from 'react';
 import { Socket } from 'socket.io-client';
+import styled from 'styled-components';
 import { useAppSelector } from '../app/hooks';
-import { selectGameRoomHostId, selectMe, selectOriginalPhrase, selectVotes } from '../features/gameRoom/gameRoomSlice';
+import {
+  selectGameRoomHostId,
+  selectGameRoomPlayers,
+  selectMe,
+  selectVotes,
+  selectVotingOptions,
+} from '../features/gameRoom/gameRoomSlice';
+import ActionButton from './ActionButton';
+
+const Container = styled.div`
+  height: 100%;
+  padding: 1.5em;
+  box-sizing: border-box;
+  display: grid;
+  grid-template-rows: auto auto 1fr auto;
+  grid-template-areas:
+    'header'
+    'heading'
+    'votes'
+    'button';
+  overflow: scroll;
+`;
+
+const Heading = styled.h2`
+  margin: 0;
+  color: #ffffff;
+  text-transform: uppercase;
+  font-weight: 400;
+  font-size: 2em;
+  margin-bottom: 1em;
+`;
+
+const PhrasesList = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  color: #ffffff;
+`;
+
+const PhrasesListItem = styled.li``;
+
+const PhraseHeading = styled.h3`
+  font-size: 1.1em;
+  font-weight: 400;
+`;
+
+const Phrase = styled.div`
+  border: 1px solid #191d21;
+  border-radius: 8px;
+  padding: 1em;
+  font-size: 0.9em;
+`;
+
+const PhraseText = styled.p`
+  margin: 0 0 1em 0;
+`;
+
+const VotesList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+`;
+
+const VotesListItem = styled.li`
+  background-color: #56616d;
+  color: #000000;
+  padding: 0.3em 0.5em;
+  border-radius: 0.3em;
+  margin-right: 1em;
+`;
 
 interface Props {
   socket: Socket;
 }
 
 export default function VotingResultsScreen({ socket }: Props) {
-  const hostId = useAppSelector(selectGameRoomHostId);
-  const me = useAppSelector(selectMe);
-  const originalPhrase = useAppSelector(selectOriginalPhrase);
-  const votes = useAppSelector(selectVotes);
+  const hostId = useAppSelector(selectGameRoomHostId)!;
+  const me = useAppSelector(selectMe)!;
+  const players = useAppSelector(selectGameRoomPlayers)!;
+  const votingOptions = useAppSelector(selectVotingOptions)!;
+  const votes = useAppSelector(selectVotes)!;
 
   const handleNextRoundButton = () => {
     socket.emit('START_NEXT_ROUND');
   };
 
   return (
-    <div>
-      <p>Original: {originalPhrase?.text}</p>
-      {votes && (
-        <ul>
-          {votes.map((vote) => (
-            <li>
-              {vote.playerId} - {vote.phrase.playerId} - {vote.phrase.text}
-            </li>
-          ))}
-        </ul>
-      )}
-      {me && me.id === hostId ? <button onClick={handleNextRoundButton}>Next round</button> : null}
-    </div>
+    <Container>
+      <header></header>
+      <Heading>Voting results</Heading>
+
+      <PhrasesList>
+        {votingOptions.map((phrase) => (
+          <PhrasesListItem>
+            <PhraseHeading>{players.find((player) => player.id === phrase.playerId)!.username}</PhraseHeading>
+            <Phrase>
+              <PhraseText>{phrase.text}</PhraseText>
+              <VotesList>
+                {votes
+                  .filter((vote) => vote.phrase.playerId === phrase.playerId)
+                  .map((vote) => (
+                    <VotesListItem>{players.find((player) => player.id === vote.playerId)!.username}</VotesListItem>
+                  ))}
+              </VotesList>
+            </Phrase>
+          </PhrasesListItem>
+        ))}
+      </PhrasesList>
+
+      {me && me.id === hostId ? <ActionButton onClick={handleNextRoundButton}>Next round</ActionButton> : null}
+    </Container>
   );
 }
